d07f70f8e133ca350310a14456b9133f9b5c0a55
ThaisG092 <thaisgaid@hotmail.com>
2024-11-29 17:50:58 -0300

    erro de arquivo

--- 3_Introducao_Back_End/Aula_23_24/servidor-basico/middlewares/logger.js
+++
@@ -1,5 +0,0 @@
-const logger = require('./middlewares/logger');
-const autenticar = require('./middlewares/autenticar');
-
-app.use(logger); // Middleware global
-app.use('/usuarios', autenticar, usuariosRouter); // Middleware específico
